c4084667f2a58fa1db09feb05981c906d5ab5911
adrien chaix <adrien.chaix@bleemeo.com>
2016-11-02 16:40:57 +0100

    test google

--- index.js
+++ index.js
@@ -295,6 +295,6 @@ function searchThis(id, text) {
   if (text.toLowerCase().indexOf("search") !== -1) {
     search = text.split("seach")[1]
   } else search = text.split("cherche")[1]
-  textMessage(id, "https://www.google.com/search?q=+rebord+arrondi+droit&btnI=")
+  textMessage(id, "https://www.google.com/search?q=" + encodeURI(search) +"&btnI=")
   return true
 }
